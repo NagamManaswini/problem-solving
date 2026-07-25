a4ce83430a5311ecfa576f3f1413f8a678ba2e21
Manaswini <nagammanaswini@gmail.com>
2026-07-25 23:14:01 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (51.54%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.py
@@ -1,11 +1,4 @@
 class Solution:
     def check(self, nums: List[int]) -> bool:
-        n = len(nums)
-
-        drops = 0
-
-        for i in range(n):
-            if nums[i] > nums[(i + 1) % n]:
-                drops += 1
-
-        return drops <= 1      
+        return sum(nums[i - 1] > nums[i] for i in range(len(nums))) < 2
+        
