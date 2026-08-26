5011c0af74bb1d08bc39b42feb8a78228679dbb6
Manaswini <nagammanaswini@gmail.com>
2026-08-26 22:45:19 +0530

    Time: 4 ms (2.89%) | Memory: 19.2 MB (95.79%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,7 +3,7 @@ class Solution:
         for i in range(n):
             nums1[m+i]=nums2[i]
         total=m+n
-        for i in range (total):
+        for i in range(total):
             for j in range(i+1,total):
                 if nums1[i]>nums1[j]:
                     nums1[i],nums1[j]=nums1[j],nums1[i]
