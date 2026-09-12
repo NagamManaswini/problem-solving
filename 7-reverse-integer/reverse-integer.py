2d3d0a1c14549df962d78eeb35256c66b111f0cd
Manaswini <nagammanaswini@gmail.com>
2026-09-12 19:55:17 +0530

    Time: 41 ms (83.17%) | Memory: 19.2 MB (31.97%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -5,6 +5,8 @@ class Solution:
             rev=int(str(x)[1:][::-1])*-1
         else:
             rev=int(str(x)[::-1])
-        if rev>2**31-1 or rev <-2**31:
-            return 0
+        if rev>2**31 -1 or rev<-2**31:
+            rev= 0
         return rev
+        
+       
